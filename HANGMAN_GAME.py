114c4c8f3b8d1f4ae86fc7c75019dd3d8a29d2fe
23211A7274 <23211A7274@bvrit.ac.in>
2024-12-02 12:54:06 +0530

    Update HANGMAN_GAME.py

--- HANGMAN_GAME.py
+++ HANGMAN_GAME.py
@@ -1,8 +1,19 @@
 import random
 
 # List of possible words for the game
-word_list = ["python", "programming", "hangman", "challenge", "developer", "computer", "code","alpha","code"]
-
+word_list = [
+    "acorn", "apple", "astronaut", "balloon", "banana", "bicycle", "broccoli", "cactus", 
+    "cat", "chocolate", "cookie", "computer", "donut", "dragon", "elephant", "envelope", 
+    "fire", "firefly", "frog", "friendship", "galaxy", "giraffe", "guitar", "happiness", 
+    "helicopter", "hotdog", "house", "iceberg", "igloo", "imagination", "island", "jelly", 
+    "jellybean", "jellyfish", "kangaroo", "kiwi", "lantern", "lemonade", "lion", "monkey", 
+    "mountain", "microwave", "night", "nightingale", "notebook", "octopus", "orange", "oven", 
+    "pizza", "planet", "pumpkin", "quill", "queen", "rainbow", "rocket", "snake", "star", 
+    "telescope", "tiger", "tree", "turtle", "unicorn", "umbrella", "vacation", "vehicle", 
+    "violin", "volcano", "water", "whale", "wizard", "x-ray", "xylophone", "yellow", 
+    "yellowstone", "zebra", "zipper", "balloon", "bicycle", "cactus", "dragon", "eggplant", 
+    "gorilla", "hound", "imagination", "lantern"
+]
 def choose_word():
     """Randomly selects a word from the word list."""
     return random.choice(word_list)
