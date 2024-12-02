8277053cf80a5ab2c2590a991bc9d0b4d041d6fc
23211A7274 <23211A7274@bvrit.ac.in>
2024-12-02 13:11:24 +0530

    Update wordslist.py

--- wordslist.py
+++ wordslist.py
@@ -1,13 +1,48 @@
 word_list = [
-    "acorn", "apple", "astronaut", "balloon", "banana", "bicycle", "broccoli", "cactus", 
-    "cat", "chocolate", "cookie", "computer", "donut", "dragon", "elephant", "envelope", 
-    "fire", "firefly", "frog", "friendship", "galaxy", "giraffe", "guitar", "happiness", 
-    "helicopter", "hotdog", "house", "iceberg", "igloo", "imagination", "island", "jelly", 
-    "jellybean", "jellyfish", "kangaroo", "kiwi", "lantern", "lemonade", "lion", "monkey", 
-    "mountain", "microwave", "night", "nightingale", "notebook", "octopus", "orange", "oven", 
-    "pizza", "planet", "pumpkin", "quill", "queen", "rainbow", "rocket", "snake", "star", 
-    "telescope", "tiger", "tree", "turtle", "unicorn", "umbrella", "vacation", "vehicle", 
-    "violin", "volcano", "water", "whale", "wizard", "x-ray", "xylophone", "yellow", 
-    "yellowstone", "zebra", "zipper", "balloon", "bicycle", "cactus", "dragon", "eggplant", 
-    "gorilla", "hound", "imagination", "lantern"
-]
+    # 2-letter words
+    "ab", "ad", "ae", "ag", "ah", "ai", "al", "am", "an", "ar", 
+    "as", "at", "aw", "ax", "ay", "ba", "be", "bi", "bo", "by", 
+    "da", "do", "ed", "ef", "eh", "el", "em", "en", "er", "es", 
+    "et", "ex", "fa", "go", "ha", "he", "hi", "ho", "id", "if", 
+    "ig", "ii", "ik", "il", "im", "in", "io", "ip", "is", "it", 
+    "jo", "ka", "la", "li", "lo", "ma", "me", "mi", "mu", "my", 
+    "na", "no", "nu", "od", "oe", "of", "oh", "oi", "om", "on", 
+    "op", "or", "os", "ow", "ox", "oy", "pa", "pe", "pi", "re", 
+    "sh", "si", "so", "ta", "to", "uh", "um", "un", "up", "us", 
+    "ut", "we", "wo", "xi", "ye", "yo",
+
+    # 3-letter words
+    "ant", "bat", "cat", "dog", "egg", "fan", "hat", "jet", "kit", "log",
+    "man", "net", "opt", "pat", "rat", "sat", "tan", "van", "wet", "yet", 
+    "zoo", "ape", "bee", "car", "dog", "ear", "fan", "gas", "hat", "jam", 
+    "kit", "lip", "man", "nap", "opt", "pat", "pen", "rat", "sea", "tap", 
+    "urn", "vet", "wig", "yet", "zip", "act", "bet", "cot", "dot", "eat", 
+    "fit", "got", "hit", "it", "jot", "lot", "mot", "not", "opt", "pot", 
+    "put", "rat", "sit", "tot", "vat", "wet", "yet", "zit", "ape", "bye", 
+    "cat", "dot", "egg", "fig", "gig", "hot", "man", "nap", "pan",
+
+    # 4-letter words
+    "able", "bake", "cape", "date", "earn", "face", "gain", "hate", "iron", "jack",
+    "king", "lake", "made", "name", "open", "pace", "quiz", "rain", "safe", "tape", 
+    "urge", "vote", "wake", "yarn", "zinc", "area", "belt", "coat", "duck", "earl", 
+    "fair", "gear", "heel", "icon", "joke", "kill", "leaf", "mine", "node", "oval", 
+    "park", "quiz", "rack", "star", "tune", "use", "very", "wait", "zero", "yoga", 
+    "about", "bore", "chat", "deal", "eagle", "fan", "grow", "hole", "idea", "jail", 
+    "kite", "leaf", "maze", "note", "okay", "pool", "rate", "safe", "tent", "unit",
+    "vibe", "west", "xmas", "year",
+
+    # 5-letter words
+    "apple", "baker", "candy", "deal", "echo", "fan", "grape", "house", "inbox", "jolly", 
+    "kite", "lamp", "mango", "newly", "ocean", "page", "quiz", "rate", "silent",
+    "tiger", "very", "yes", "wind", "zone", "urban", "vibe", "track", "waltz",
+    "lotus", "bats", "play", "sword", "magic", "worth", "carry", "wilds", "stain",
+    "start", "thing", "value", "voice", "woman", "zebra", "beach", "block", "climb",
+    "dance", "earth", "fall", "good", "half", "idea", "joke", "kill", "lake",
+    "make", "noise", "park", "part", "quit", "rain", "safe", "tune", "zero",
+
+    # 6-letter words
+    "baking", "cherry", "dogged", "entry", "flame", "grape", "harder", "intake", "jacket", 
+    "loved", "magic", "named", "paint", "quilt", "road", "silence", "ticket", "under",
+    "vein", "zone", "bomb", "list", "trip", "live", "jump", "beat", "play", "start",
+    "wild", "swank", "light", "think", "happy"
+    ]
